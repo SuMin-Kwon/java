32c1750a8e56b9e80fae35ec8fcbebb43d545fa8
수민 <rnjstnals120@naver.com>
2021-06-24 23:43:42 +0900

    백준

--- java.book/src/백준/Jayeunsu.java
+++ java.book/src/백준/Jayeunsu.java
@@ -1,35 +1,43 @@
 package 백준;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//import java.util.Scanner;
 
 public class Jayeunsu {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		Scanner scanner = new Scanner(System.in);
+		int n = Integer.parseInt(br.readLine());
 		
 		
-		int result;
-		int count = 0;
-		int nums = scanner.nextInt(); // 숫자 입력받음
-		int [] number = new int[nums]; // 배열 생성
-		
-		for (int i =0; i < nums; i++ ) { // 배열의 자연수 생성
-			number[i] = i+1;
-		}
-		
-		while(true) {
-				int nansu = 0;
-				result = number[nansu];
-				if( result == nums ) {
-					count = count + 1;
-				}else if (result < nums) {
-					nansu++;
-					continue;
-				}else {
-					break;
-				}
-			}
-		}
-
+	//	Scanner scanner = new Scanner(System.in);
+	//	
+	//	
+	//	int result;
+	//	int count = 0;
+	//	int nums = scanner.nextInt(); // 숫자 입력받음
+	//	int [] number = new int[nums]; // 배열 생성
+	//	
+	//	for (int i =0; i < nums; i++ ) { // 배열의 자연수 생성
+	//		number[i] = i+1;
+	//	}
+	//	
+	//	while(true) {
+	//			int nansu = 0;
+	//			result = number[nansu];
+	//			if( result == nums ) {
+	//				count = count + 1;
+	//			}else if (result < nums) {
+	//				nansu++;
+	//				continue;
+	//			}else {
+	//				break;
+	//			}
+	//		}
+	}
 }
