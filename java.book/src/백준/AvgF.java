b8e5e0d7bcffe7bbc49ccc29af7c5aab0468aa77
수민 <rnjstnals120@naver.com>
2021-06-27 11:08:23 +0900

    백준문제

--- java.book/src/백준/AvgF.java
+++ java.book/src/백준/AvgF.java
@@ -10,21 +10,34 @@ public class AvgF {
 		Scanner scLine = new Scanner(System.in);
 		
 		int N = sc.nextInt();
-		int [] result = new int[N];
 		int sum = 0;
-		double avg;
-		
+		int avg;
+		int count=0;
+		double beyul;
 		
 		for (int i = 1; i <= N; i++ ) {
+			sum = 0;
+			count = 0;
+			avg = 0;
 			String a1 = scLine.nextLine();
 			int jumsu = Integer.parseInt(a1.substring(0,1));
 			String [] sums = a1.split(" ");
-			for (int j=0; j <jumsu; j++) {
+			
+			for (int j=1; j <= jumsu; j++) {
 				sum = sum + Integer.parseInt(sums[j]);
 			}
-			avg = 0;
+			avg = sum / jumsu;
+			for (int e=1; e <= jumsu; e++ ) {
+				if(avg < Integer.parseInt(sums[e])) {
+					count++;
+				}
+			}
+			beyul = (count*100)/(double)jumsu;	// 강제 형변환
+			System.out.println(beyul);
+			System.out.println(String.format("%.3f", beyul));
 		}
 		
+
 		
 	}
 
