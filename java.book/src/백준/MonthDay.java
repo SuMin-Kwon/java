60773f489a5cfb88c348e7c6b9dd95a40c216796
admin <rnjstnals1202naver.com>
2021-06-28 17:43:22 +0900

    백준

--- java.book/src/백준/MonthDay.java
+++ java.book/src/백준/MonthDay.java
@@ -4,6 +4,12 @@ public class MonthDay {
 
 	public static void main(String[] args) {
 		
+		// 기본날짜를 2007년 1월 1일 월요일로 맞추고
+		// 거기서 부터 일수에 7을 나눔 그리고 나머지값으로 요일 구별
+		// 월별을 if문으로 조건을 둠
+		
+		
+		
 	}
 
 }
