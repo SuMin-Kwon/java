d9b65f84b2223199f1a12daa808131a9e0d68bad
수민 <rnjstnals120@naver.com>
2021-07-01 21:26:22 +0900

    백준

--- java.book/src/백준/MonthDay.java
+++ java.book/src/백준/MonthDay.java
@@ -1,5 +1,10 @@
 package 백준;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class MonthDay {
 
 	public static void main(String[] args) {
@@ -7,6 +12,13 @@ public class MonthDay {
 		// 기본날짜를 2007년 1월 1일 월요일로 맞추고
 		// 거기서 부터 일수에 7을 나눔 그리고 나머지값으로 요일 구별
 		// 월별을 if문으로 조건을 둠
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String date = sc.next();
+		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.KOREA);
+		LocalDate gijundate = LocalDate.parse(date, formatter);
 		
 		
 		
